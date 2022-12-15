cb9ba3c16c033c3afcff5ca6606e1b9112931d74
Rafael Aldana <rafaelaldana6@Gmail.com>
2022-12-15 15:59:01 -0800

    added local storage

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@ Cart.prototype.addItem = function (product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
-
-};
+  localStorage.setItem('cartData', JSON.stringify(this.items));
+};clear
 
 Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
